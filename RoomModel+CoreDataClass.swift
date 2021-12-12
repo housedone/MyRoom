3826877aabd2b449c350e5a3011b86dbe12db0fd
housedone <kimws930@gmail.com>
2021-12-12 22:28:00 +0900

    CoreData 변경과정

---
+++ RoomModel+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  RoomModel+CoreDataClass.swift
+//  MyRoom
+//
+//  Created by 김우성 on 2021/12/12.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(RoomModel)
+public class RoomModel: NSManagedObject {
+
+}
